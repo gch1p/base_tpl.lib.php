6433bb1409f22036d01a470572edb425f91a3f1e
evgeny <me@ch1p.com>
2016-08-14 23:47:01 +0200

    :retab

--- tpl.lib.php
+++ tpl.lib.php
@@ -9,23 +9,23 @@ class tpl {
         $this->templateDir = $templateDir;
     }
 
-	public function set($arg1, $arg2 = null) {
-		if (is_array($arg1)) {
-			foreach ($arg1 as $key => $value) {
-				$this->varsStack[0][$key] = $value;
-			}
-		} elseif ($arg2 !== null) {
-			$this->varsStack[0][$arg1] = $arg2;
-		}
+    public function set($arg1, $arg2 = null) {
+        if (is_array($arg1)) {
+            foreach ($arg1 as $key => $value) {
+                $this->varsStack[0][$key] = $value;
+            }
+        } elseif ($arg2 !== null) {
+            $this->varsStack[0][$arg1] = $arg2;
+        }
     }
 
-	public function fetch($template, array $vars = []) {
+    public function fetch($template, array $vars = []) {
         global $config;
 
         if (!$this->templateDir) throw new Exception('templateDir is not set');
 
         $template = $this->templateDir.'/'.$template;
-		if (!file_exists($template)) throw new Exception($template.' does not exist!');
+        if (!file_exists($template)) throw new Exception($template.' does not exist!');
 
         if (!empty($vars)) $this->varsStack[] = $vars;
 
@@ -33,17 +33,17 @@ class tpl {
             if (!empty($_vars)) extract($_vars);
         }
 
-		ob_start();
+        ob_start();
 
         include $template;
 
         array_pop($this->varsStack);
 
-		return ob_get_clean();
-	}
+        return ob_get_clean();
+    }
 
     public function display($template, array $vars = []) {
-		echo $this->fetch($template, $vars);
+        echo $this->fetch($template, $vars);
     }
 
 }
